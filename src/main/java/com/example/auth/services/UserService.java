dbf630361180acbead83ee3c8ea2466dcb184151
Daria Kozina <kozina.flowers@yandex.ru>
2026-04-22 09:39:49 +0300

    prepared user service for future dev

--- src/main/java/com/example/auth/services/UserService.java
+++ src/main/java/com/example/auth/services/UserService.java
@@ -1,10 +1,7 @@
 package com.example.auth.services;
 
-import com.example.auth.dtos.users.UserRequest;
 import com.example.auth.dtos.users.UserResponse;
 import com.example.auth.entities.User;
-import com.example.auth.entities.Book;
-import com.example.auth.exceptions.InvalidRequestException;
 import com.example.auth.exceptions.ResourceNotFoundException;
 import com.example.auth.mappers.UserMapper;
 import com.example.auth.repositories.UserRepository;
@@ -13,11 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -25,39 +17,17 @@ public class UserService {
     private final UserMapper userMapper;
     private final BookRepository bookRepository;
 
-    @Transactional
-    public UserResponse create(UserRequest request) {
-        User user = userMapper.toEntity(request);
-        Set<Long> requestIds = request.getBookIds();
-        if (!requestIds.isEmpty()) {
-            List<Book> activeBooks = bookRepository.findActiveByIds(request.getBookIds());
-            checkBooksOfRequest(activeBooks, requestIds);
-            user.setBooks(new HashSet<>(activeBooks));
-        } else {
-            user.setBooks(new HashSet<>());
-        }
-        User saved = userRepository.save(user);
-        return userMapper.toResponse(saved);
-    }
+//    @Transactional(readOnly = true)
+//    public UserResponse getMe(Long userId) {
+//        User user = userRepository.findActiveById(userId).orElseThrow(
+//                () -> new ResourceNotFoundException("User does not exist or deleted with such id " + userId)
+//        );
+//        return userMapper.toResponse(user);
+//    }
 
     @Transactional(readOnly = true)
-    public UserResponse getOne(Long authorId) {
-        User user = userRepository.findActiveById(authorId).orElseThrow(
-                () -> new ResourceNotFoundException("Author does not exist or deleted with such id " + authorId)
-        );
-        return userMapper.toResponse(user);
-    }
+    public String getMe(Long userId) {
 
-    private void checkBooksOfRequest(List<Book> activeBooks, Set<Long> requestIds) {
-        if (activeBooks.size() < requestIds.size()) {
-            Set<Long> foundIds = activeBooks.stream()
-                    .map(Book::getId)
-                    .collect(Collectors.toSet());
-
-            Set<Long> invalidIds = requestIds.stream()
-                    .filter(id -> !foundIds.contains(id))
-                    .collect(Collectors.toSet());
-            throw new InvalidRequestException("Some books do not exist or were deleted. Invalid ids are: " + invalidIds);
-        }
+        return "user found with id " + userId;
     }
 }
